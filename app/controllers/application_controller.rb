af3ed20a00a47bfefb1eb6c874362748c66c8bf2
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-04-30 19:03:18 +0530

    FORCE HTTPS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_https
+
+  def set_https
+    if Rails.env != "development"
+      unless request.url.starts_with?('https')
+        redirect_to request.url.gsub('http', 'https') and return
+      end
+    end
+  end
+
   def authorize
     unless current_user
       render plain: 'unauthorized' and return
